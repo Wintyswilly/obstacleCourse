eca22bdf262682433ad3c3b9077872fe2476b165
Wintyswilly <Wintyboss@outlook.com>
2021-04-03 00:54:31 +0200

    Variables and materials

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0.01f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,7 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
 
     }
 }
